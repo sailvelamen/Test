c6451a81ac5199b25259719b56c750e1374a98ab
sailvelamen <jifan1914346052@qq.com>
2020-07-08 18:23:48 +0800

    master

---
+++ 01helloworld.py
@@ -0,0 +1,21 @@
+print("helloworld")
+message = "hello world"
+print(message)
+
+name = "ji fan"
+print(name)
+print(name.title()) #字符串首字母大写
+print(name.upper()) #字符串全部大写
+print(name.lower()) #字符串全部小写
+
+print("\thello\tworld\nhellowprld")
+print('helloworld,"zhangsan"')
+
+a = 3 * 8 + 7
+print(a)
+b = 0.2 + 0.6
+print(b)
+
+age = 18
+message = 'happy ' + str(age) + 'rd birthday' #str()使数字转换为字符串
+print(message)
